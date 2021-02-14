d161046a0b06e34267fc58e977e76e246051de81
Alexander Levitsky <piro@list.ru>
2021-02-14 16:17:08 +0300

    init project structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,9 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  .page
+    h3 hello world from home
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
