5b3cb0c89474d4729bfa8ecf86298510d604456e
MaxXam715 <Hertam1@2>
2021-02-27 15:32:31 +0300

    npm install materialize-css@next

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum delectus laborum officiis sed velit veritatis! Accusantium cum dolore est illum, iure magni neque placeat reprehenderit sint, velit vero vitae.</div>
+    
+    <div class="gallery">
+      <img src="https://wallpaperscave.ru/images/original/18/08-15/sports-motocross-76074.jpg" alt="">
+      <img src="https://img.youscreen.ru/wall/14977236643469/14977236643469_1920x1200.jpg" alt="">
+      <img src="https://wallpaperscave.ru/images/original/18/02-21/sports-motocross-19774.jpg" alt="">
+    </div>
+    
   </div>
 </template>
 
@@ -9,5 +15,18 @@
 </script>
 
 <style lang="less">
+
+.gallery {
+  display: grid;
+  grid-template-columns: 1fr;
+  width: 100%;
+  height: auto;
+
+  img {
+    width: inherit;
+    height: 100%;
+    object-fit: cover;
+  }
+}
 
 </style>
